096c5a5c0667f4885a2a290dbdb9a67d9ee2057d
MidnytRaccoon <30532399+MidnytRaccoon@users.noreply.github.com>
2025-02-13 16:34:36 +0800

    Added Movement
    Added the getInput Script.

--- The Labyrinth/Assets/player.cs
+++ The Labyrinth/Assets/player.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField] public int health;
+    [SerializeField] public int ammo;
+    [SerializeField] public float reloadSpeed;
+    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] private Vector3 moveDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<Renderer>().material.color = Color.orange;
+        health = 100;
+        ammo = 50;
+        reloadSpeed = 1f;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        getInput();
+    }
+    public void getInput(){
+    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
     }
 }
